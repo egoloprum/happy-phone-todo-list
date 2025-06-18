dea965612a0ae9bdd7bc7a0634df82d7e5edc5b5
egoloprum <bayraa093@gmail.com>
2025-06-18 09:01:49 +0300

    delete-task function is implemented

--- src/features/task/ui/deleteTaskBtn.tsx
+++ src/features/task/ui/deleteTaskBtn.tsx
@@ -1,19 +1,51 @@
 'use client'
 
 import { X } from 'lucide-react'
-import { FC } from 'react'
+import { useRouter } from 'next/navigation'
+import { FC, useEffect, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
 
 import { Button } from '@/shared/ui'
+
+import { DeleteTaskModal } from './deleteTaskModal'
 
 interface DeleteTaskBtnProps {
   taskId: number
 }
 
 export const DeleteTaskBtn: FC<DeleteTaskBtnProps> = ({ taskId }) => {
+  const dialogRef = useRef<{ showModal: () => void; close: () => void } | null>(
+    null
+  )
+  const [isClient, setIsClient] = useState<boolean>(false)
+
+  useEffect(() => setIsClient(true), [])
+
+  const openDialog = () => dialogRef.current?.showModal()
+  const closeDialog = () => dialogRef.current?.close()
+
+  const router = useRouter()
+
+  const handleDeleteTask = () => {
+    router.push('/')
+  }
+
   return (
-    <Button type="button" mode="danger">
-      <X />
-      Delete
-    </Button>
+    <>
+      <Button type="button" mode="danger" onClick={openDialog}>
+        <X />
+        Delete
+      </Button>
+      {isClient &&
+        createPortal(
+          <DeleteTaskModal
+            onClose={closeDialog}
+            ref={dialogRef}
+            taskId={taskId}
+            onDelete={handleDeleteTask}
+          />,
+          document.body
+        )}
+    </>
   )
 }
